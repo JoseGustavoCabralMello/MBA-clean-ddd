2181689c60b74ae5640bd6f0c4532a27741768a9
JoseGustavoCabralMello <gu.mello.gm@gmail.com>
2025-02-21 20:41:57 -0300

    Fundamentos de DDD - Path aliases e Vitest globals

--- src/domain/use-cases/answer-question.spec.ts
+++ src/domain/use-cases/answer-question.spec.ts
@@ -1,4 +1,3 @@
-import { expect, test } from "vitest"
 import { AnswerQuestionUseCase } from "./answer-question"
 import { AnswersRepository } from "../repositoreis/answers-repository";
 import { Answer } from "../entities/answer";
